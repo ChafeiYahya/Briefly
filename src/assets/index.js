02626860aebea195c198bf7d2f22b2ab9f3bcd7a
ChafeiYahya <yahya.chafei99@gmail.com>
2023-04-23 15:04:37 +0100

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.png'
+import tick from './tick.svg'
+import enter from './enter.png'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    enter
+}
